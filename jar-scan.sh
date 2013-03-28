f4c1798254019f1e0adf26c6c20482fc85874b66
Ron Kitay <ronkitay@gmail.com>
2013-03-28 16:01:50 +0200

    First version

---
+++ jar-scan.sh
@@ -0,0 +1,24 @@
+#!/bin/tcsh -f
+
+if ( $#argv < 2 ) then
+        printf "Scans all the JARs in the specified directory for classes matching the specified string\n"
+        printf "Usage: $0 <dir> <class-name-pattern>\n"
+        printf "Example: $0 . Exception\n"
+        printf 'Example: '$0' $JAVA_HOME/lib "com\/sun\/.*action"\n'
+        exit 1
+endif
+
+set DIR = "$1"
+set CLASS = "$2"
+
+foreach J ( `find $DIR -name "*.jar"` )
+        printf "."
+        set res=`jar tvf $J | grep "$CLASS" | sed 's/$/;/'`
+        set outputsize = `echo $res|wc -c`
+        if ( $outputsize > 1 )  then
+                printf "\nMatch found in [$J]\n "
+                printf  "$res\n" | tr ';' '\n'
+        endif
+end
+
+printf "\n"
